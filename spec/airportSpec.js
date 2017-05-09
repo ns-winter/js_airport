dc440318e0e2e5ffe6351582ac1f05297550bb86
Nigel Said <nigelsaid@gmail.com>
2017-05-09 20:28:19 +0100

    adds airport capacity

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe("Airport", function() {
   var airport;
   var plane;
@@ -8,22 +10,39 @@ describe("Airport", function() {
   });
 
   it("should land a plane", function() {
-    spyOn(airport, '_checkweather');
+    spyOn(airport, '_checkWeather');
     airport.land(plane)
     expect(airport.hangar).toContain(plane)
   });
 
   it("should takeoff a plane", function() {
-    spyOn(airport, '_checkweather');
+    spyOn(airport, '_checkWeather');
     airport.land(plane)
     airport.takeoff(plane)
     expect(airport.hangar).not.toContain(plane)
   });
 
-  it("if weather is stormy, prevents takeoff and landing", function() {
-    spyOn(airport, '_checkweather').and.throwError("Stormy Weather");
+  it("should prevent plane from landing if", function() {
+    spyOn(airport, '_checkWeather');
+    airport.land(plane)
+    airport.land(plane)
+    expect (function(){
+       airport.land(plane) }).toThrowError("Airport Full")
+  });
+
+  it("if weather is stormy, prevent landing", function() {
+    spyOn(airport, '_checkWeather').and.throwError("Stormy Weather");
     expect(function() {
-      airport._checkweather()}).toThrowError("Stormy Weather");
+      airport.land(plane)}).toThrowError("Stormy Weather");
+      expect(airport.hangar).not.toContain(plane);
+  });
+
+  it("if weather is stormy, prevent takeoff", function() {
+    airport.hangar = ['plane']
+    spyOn(airport, '_checkWeather').and.throwError("Stormy Weather");
+    expect(function() {
+      airport.takeoff(plane)}).toThrowError("Stormy Weather");
+      expect(airport.hangar).toContain(plane);
   });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -1,19 +1,25 @@
 function Airport() {
   this.hangar = [];
+  this.hangarCapacity = 2
 };
 
 Airport.prototype.land = function(plane) {
-  this._checkweather();
+  this._capacityCheck();
+  this._checkWeather();
   this.hangar.push(plane);
 };
 
 
 Airport.prototype.takeoff = function(plane) {
-  this._checkweather()
+  this._checkWeather()
   this.hangar.pop(plane);
 };
 
-Airport.prototype._checkweather = function(){
+Airport.prototype._checkWeather = function(){
 var weather = (new Weather).condition()
-if (weather === "Stormy") throw "Stormy Weather";
+if (weather === "Stormy") throw(new Error("Stormy Weather"));
+};
+
+Airport.prototype._capacityCheck = function(){
+if (this.hangar.length >= this.hangarCapacity) throw(new Error("Airport Full"));
 };
